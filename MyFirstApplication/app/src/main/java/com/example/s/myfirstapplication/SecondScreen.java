5ff1cb309b9e7f9f963f108df31d2157dd85d775
HotlineHenok <hktesfai@gmail.com>
2015-10-31 01:22:00 +0100

    My first Android App test

---
+++ MyFirstApplication/app/src/main/java/com/example/s/myfirstapplication/SecondScreen.java
@@ -0,0 +1,16 @@
+package com.example.s.myfirstapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by S on 30-10-2015.
+ */
+public class SecondScreen extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
